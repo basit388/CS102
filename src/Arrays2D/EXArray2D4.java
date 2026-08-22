05aeb38e6178dbf97b1b47239fdd32a4c210c750
basit <basit@localhost>
2026-08-22 17:59:26 +0300

    updated exercise4

---
+++ src/Arrays2D/EXArray2D4.java
@@ -0,0 +1,41 @@
+package Arrays2D;
+
+/*
+==========================2026 (c) Basit Qureshi ===========================
+
+CS102 Programming II
+Dept. of Computer Sc, Prince Sultan University
+August 8, 2026
+https://www.ieeepsu.org/basit/cs102/
+
+//Exercise 4
+Write a program that asks the user for dimensions of a 2-D array.
+It processes information randomly filled in the array by calling
+methods:
+-Write a fillArray() method that takes the 2-D array as parameter. 
+ It fills the array with random values and returns void.
+-Write a printArray() method that prints all the elements in the
+ array as a matrix.
+-Write a makeArray() method that takes two parameters int x and y.
+ It makes an array of type double with x, y as rows and columns and
+ returns the array.
+-Write a method findMaxR(int r) that returns the largest value in the
+ row r of the array.
+-Write a method findMinC(int c) that returns the smallest value in the
+ column c of the array.
+
+*/
+
+
+public class EXArray2D4 {
+    public static void main(String [] s)
+    {
+
+        
+        
+
+
+        
+    }
+    
+}
